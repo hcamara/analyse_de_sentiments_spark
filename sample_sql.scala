8501c6f0eec300e4ef867026794f3c6955f098bb
Hamadi camara <hamadi.camara@outlook.com>
2025-04-02 07:51:19 +0200

    Udpate

---
+++ sample_sql.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.sql.SparkSession
+
+val spark = SparkSession.builder()
+  .appName("Spark SQL Example")
+  .master("local[*]")
+  .getOrCreate()
+
+// Charger des données en DataFrame
+val df = spark.read.option("header", "true").csv("sentiments.csv")
+
+// Exécution d'une requête SQL
+df.createOrReplaceTempView("data_table")
+val result = spark.sql("SELECT column1, column2 FROM data_table WHERE column3 > 100")
+
+// Afficher les résultats
+result.show()
+
+spark.stop()
