b4429a5f1236c32291b05148f6e276b0a7f81b12
Hamadi camara <hamadi.camara@outlook.com>
2025-04-02 07:11:14 +0200

    Udpate

--- spark_sentiment_analysis.scala
+++ spark_sentiment_analysis.scala
@@ -9,70 +9,70 @@ import org.apache.spark.sql.functions._
 object SentimentAnalysis {
   def main(args: Array[String]): Unit = {
 
-// Create Spark Session
-val spark = SparkSession.builder()
-  .appName("Sentiment Analysis")
-  .master("local[*]")
-  .getOrCreate()
+    // Create Spark Session
+    val spark = SparkSession.builder()
+      .appName("Sentiment Analysis")
+      .master("local[*]")
+      .getOrCreate()
 
-println(spark)
-// Load Data
+    println(spark)
+    // Load Data
 
-val data = spark.read.option("header", "true")
-  .option("inferSchema", "true")
-  .csv("./sentiments.csv")
-  .toDF("label", "text")
+    val data = spark.read.option("header", "true")
+      .option("inferSchema", "true")
+      .csv("./sentiments.csv")
+      .toDF("label", "text")
 
-data.show(5)
+    data.show(5)
 
-// Tokenization
-val tokenizer = new RegexTokenizer()
-  .setInputCol("text")
-  .setOutputCol("words")
-  .setPattern("\\W+")
+    // Tokenization
+    val tokenizer = new RegexTokenizer()
+      .setInputCol("text")
+      .setOutputCol("words")
+      .setPattern("\\W+")
 
-// Stop Words Removal
-val remover = new StopWordsRemover()
-  .setInputCol("words")
-  .setOutputCol("filtered_words")
+    // Stop Words Removal
+    val remover = new StopWordsRemover()
+      .setInputCol("words")
+      .setOutputCol("filtered_words")
 
-// TF-IDF Feature Extraction
-val hashingTF = new HashingTF()
-  .setInputCol("filtered_words")
-  .setOutputCol("raw_features")
-  .setNumFeatures(1000)
+    // TF-IDF Feature Extraction
+    val hashingTF = new HashingTF()
+      .setInputCol("filtered_words")
+      .setOutputCol("raw_features")
+      .setNumFeatures(1000)
 
-val idf = new IDF()
-  .setInputCol("raw_features")
-  .setOutputCol("features")
+    val idf = new IDF()
+      .setInputCol("raw_features")
+      .setOutputCol("features")
 
-// Logistic Regression Model
-val lr = new LogisticRegression()
-  .setMaxIter(20)
-  .setRegParam(0.01)
+    // Logistic Regression Model
+    val lr = new LogisticRegression()
+      .setMaxIter(20)
+      .setRegParam(0.01)
 
-// Create Pipeline
-val pipeline = new Pipeline()
-  .setStages(Array(tokenizer, remover, hashingTF, idf, lr))
+    // Create Pipeline
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, remover, hashingTF, idf, lr))
 
-// Split Data
-val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2), seed = 42)
+    // Split Data
+    val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2), seed = 42)
 
-// Train Model
-val model = pipeline.fit(trainingData)
+    // Train Model
+    val model = pipeline.fit(trainingData)
 
-// Evaluate Model
-val predictions = model.transform(testData)
-predictions.select("text", "label", "prediction").show(10)
+    // Evaluate Model
+    val predictions = model.transform(testData)
+    predictions.select("text", "label", "prediction").show(10)
 
-// Predict New Text
-import spark.implicits._
-val newText = Seq(("Ce produit est incroyable, je l'adore !")).toDF("text")
-val prediction = model.transform(newText)
-prediction.select("text", "prediction").show()
+    // Predict New Text
+    import spark.implicits._
+    val newText = Seq(("Ce produit est incroyable, je l'adore !")).toDF("text")
+    val prediction = model.transform(newText)
+    prediction.select("text", "prediction").show()
 
-// Stop Spark Session
-spark.stop()
+    // Stop Spark Session
+    spark.stop()
 
   }
   
